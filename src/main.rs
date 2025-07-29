430fbe46a2a16dcc91f7d8b98620a3574b9bb2a1
rwprange1 <richwp12@gmail.com>
2025-07-28 23:03:32 -0400

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod utils;
 
 use std::env::{current_dir, set_current_dir};
-use std::fmt::format;
 use std::path;
-use std::path::{PathBuf, Path};
+use std::path::PathBuf;
 
-use iced::{widget::{button, column, text, row}, window, Color, Element, Fill, Subscription, Task, Theme, keyboard};
 use iced::keyboard::key::Named;
-use iced::widget::{horizontal_rule, Column};
+use iced::widget::{Column, horizontal_rule};
+use iced::{
+    Color, Element, Fill, Subscription, Task, Theme, keyboard,
+    widget::{button, column, row, text},
+    window,
+};
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Message {
@@ -22,11 +25,10 @@ const RED: Color = Color::from_rgb(1.0, 0.0, 0.0);
 const BLUE: Color = Color::from_rgb(0.0, 0.0, 1.0);
 const GREEN: Color = Color::from_rgb(0.0, 1.0, 0.0);
 
-
 #[derive(Debug)]
 struct AppState {
     current_dir: PathBuf,
-    current_files: Vec<(String,bool)>,
+    current_files: Vec<(String, bool)>,
     selected_file: i8,
     success: bool,
     ran: bool,
@@ -35,27 +37,25 @@ struct AppState {
 impl Default for AppState {
     fn default() -> Self {
         let current_dir: PathBuf = current_dir().unwrap();
-        let current_files: Vec<(String,bool)> = utils::get_files(&current_dir);
-        
-        AppState{
+        let current_files: Vec<(String, bool)> = utils::get_files(&current_dir);
+
+        AppState {
             current_dir,
             current_files,
             selected_file: 0,
             success: false,
-            ran: false
+            ran: false,
         }
     }
 }
 
 impl AppState {
-    
-
     fn view(&self) -> Element<Message> {
         let file = format!("{} ", self.current_files[self.selected_file as usize].0);
-        if self.ran{
-            let (col,val) = if self.success {
+        if self.ran {
+            let (col, val) = if self.success {
                 (GREEN, "Successful Audio Rip")
-            }else{
+            } else {
                 (RED, "Audio Rip Failed")
             };
             return row![
@@ -63,19 +63,19 @@ impl AppState {
                 text(file).size(25).color(BLUE),
                 text(val).size(25).color(col).width(Fill),
                 button("Click Me To Return To JRIP").on_press(Message::Ret),
-               
-                
-            ].into();
-            
+            ]
+            .into();
         };
-        
+
         // Top row: current directory + Exit button
         let header = row![
-            text(self.current_dir.to_str().unwrap()).size(24).width(Fill),
+            text(self.current_dir.to_str().unwrap())
+                .size(24)
+                .width(Fill),
             button(text("Exit").size(20)).on_press(Message::Exit),
-            
-        ].spacing(8);
-        
+        ]
+        .spacing(8);
+
         let mut columns: Vec<Column<Message>> = vec![];
         let mut current_column = column![];
         let mut count = 0;
@@ -88,123 +88,102 @@ impl AppState {
             } else {
                 GREEN
             };
-            
-            current_column = current_column.push(
-                text(val.0.clone()).size(24).color(color)
-            );
+
+            current_column = current_column.push(text(val.0.clone()).size(24).color(color));
             count += 1;
 
-            
             if count == MAX_FILES {
                 columns.push(current_column);
                 current_column = column![];
                 count = 0;
             }
         }
-        
+
         if count > 0 {
             columns.push(current_column);
         }
 
-        let file_columns = row(
-            columns.into_iter().map(|col| col.into()).collect::<Vec<_>>()
-        ).spacing(16);
-        
-        column![
-            header.width(Fill),
-            horizontal_rule(2),
-            file_columns
-        ]
-        .spacing(16)
-        .into()
+        let file_columns = row(columns
+            .into_iter()
+            .map(|col| col.into())
+            .collect::<Vec<_>>())
+        .spacing(16);
+
+        column![header.width(Fill), horizontal_rule(2), file_columns]
+            .spacing(16)
+            .into()
     }
 
-
-    fn update (&mut self, msg: Message) -> Task<Message>{
-            match msg {
-                Message::Exit => {
-                    window::get_latest().and_then(window::close)
-                },
-                Message::Up => {
-                    if !self.current_files.is_empty() {
-                        if self.selected_file > 0 {
-                            self.selected_file -= 1;
-                        } else {
-                            self.selected_file = (self.current_files.len() -1) as i8;
-                        }
+    fn update(&mut self, msg: Message) -> Task<Message> {
+        match msg {
+            Message::Exit => window::get_latest().and_then(window::close),
+            Message::Up => {
+                if !self.current_files.is_empty() {
+                    if self.selected_file > 0 {
+                        self.selected_file -= 1;
+                    } else {
+                        self.selected_file = (self.current_files.len() - 1) as i8;
                     }
-                    Task::none()
-                },
-                Message::Down => {
-                    if !self.current_files.is_empty() {
-                        if self.selected_file < (self.current_files.len() -1) as i8 {
-                            self.selected_file += 1;
-                        } else {
-                            self.selected_file = 0
-                        }
-                        
-                        
-                    }
-                    Task::none()
-                },
-                Message::Enter => {
-                    let path = path::absolute(
-                        PathBuf::from(
-                            self.current_files[self.selected_file as usize].
-                                0
-                                .clone()
-                        )
-                    ).unwrap();
-                    
-                    if path.is_dir() {
-                        set_current_dir(&path).unwrap();
-                        self.current_dir = path::absolute(&path).unwrap();
-                        self.selected_file = 0;
-                        self.current_files = utils::get_files(&self.current_dir);
-                    }else{
-                        self.ran = true;
-                        self.success = utils::rip_it(
-                            &self.current_files[self.selected_file as usize].0,
-                            &self.current_dir
-                        );
-                    }
-                    Task::none()
-                },
-                Message::Ret => {
-                    self.ran = false;
-                    self.success = false;
-                    Task::none()
                 }
+                Task::none()
             }
+            Message::Down => {
+                if !self.current_files.is_empty() {
+                    if self.selected_file < (self.current_files.len() - 1) as i8 {
+                        self.selected_file += 1;
+                    } else {
+                        self.selected_file = 0
+                    }
+                }
+                Task::none()
+            }
+            Message::Enter => {
+                let path = path::absolute(PathBuf::from(
+                    self.current_files[self.selected_file as usize].0.clone(),
+                ))
+                .unwrap();
+
+                if path.is_dir() {
+                    set_current_dir(&path).unwrap();
+                    self.current_dir = path::absolute(&path).unwrap();
+                    self.selected_file = 0;
+                    self.current_files = utils::get_files(&self.current_dir);
+                } else {
+                    self.ran = true;
+                    self.success = utils::rip_it(
+                        &self.current_files[self.selected_file as usize].0,
+                        &self.current_dir,
+                    );
+                }
+                Task::none()
+            }
+            Message::Ret => {
+                self.ran = false;
+                self.success = false;
+                Task::none()
+            }
+        }
     }
 
     fn subscription(&self) -> Subscription<Message> {
         fn handle_hotkey(key: keyboard::Key, _modifiers: keyboard::Modifiers) -> Option<Message> {
             match key.as_ref() {
-                keyboard::Key::Named(x) => {
-                    match x {
-                        Named::ArrowUp => Some(Message::Up),
-                        Named::ArrowDown => Some(Message::Down),
-                        Named::Enter => Some(Message::Enter),
-                        Named::Escape => Some(Message::Exit),
-                        _ => None,
-                    }
-                }
+                keyboard::Key::Named(x) => match x {
+                    Named::ArrowUp => Some(Message::Up),
+                    Named::ArrowDown => Some(Message::Down),
+                    Named::Enter => Some(Message::Enter),
+                    Named::Escape => Some(Message::Exit),
+                    _ => None,
+                },
                 _ => None,
             }
         }
-
         keyboard::on_key_press(handle_hotkey)
     }
-
-
 }
 
-
-
-fn main() ->iced::Result {
-    println!("Hello, world!");
-    iced::application("jrip",  AppState::update, AppState::view )
+fn main() -> iced::Result {
+    iced::application("JRIP", AppState::update, AppState::view)
         .subscription(AppState::subscription)
         .theme(|_| Theme::KanagawaDragon)
         .run()
--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,15 @@
 use std::fmt::format;
-use std::fs::{read_dir, ReadDir, OpenOptions};
+use std::fs::{OpenOptions, ReadDir, read_dir};
 use std::path::PathBuf;
-use std::process::{Command};
-
+use std::process::Command;
 
 pub fn get_files(curr_dir: &PathBuf) -> Vec<(String, bool)> {
-    let mut holder: Vec<(String,bool)> = Vec::new();
+    let mut holder: Vec<(String, bool)> = Vec::new();
     let dir: ReadDir = match read_dir(curr_dir) {
         Ok(f) => f,
         Err(_) => panic!("{}", "Error Retrieving Files"),
     };
-    
+
     holder.push(("..".to_string(), true));
 
     for file in dir {
@@ -18,27 +17,25 @@ pub fn get_files(curr_dir: &PathBuf) -> Vec<(String, bool)> {
         let name = file.file_name().to_str().unwrap().to_string();
         let is_dir = file.file_type().unwrap().is_dir();
 
-        if is_dir || name.ends_with("mkv"){
+        if is_dir || name.ends_with("mkv") {
             holder.push((name.clone(), is_dir));
         }
-      
-
     }
     holder
 }
 
-
 pub fn rip_it(file: &String, parent: &PathBuf) -> bool {
     if let Ok(_) = Command::new("ffmpeg")
-        .args(
-            ["-i",
-                format!("{}\\{}",parent.to_str().unwrap_or("temp"), file ).as_str(),
-                "-y",
-                format!("{}.mp3", file.replace(".mkv", "")).as_str(),
-                "-loglevel",
-                "-8"
-            ])
-        .status(){
+        .args([
+            "-i",
+            format!("{}\\{}", parent.to_str().unwrap_or("temp"), file).as_str(),
+            "-y",
+            format!("{}.mp3", file.replace(".mkv", "")).as_str(),
+            "-loglevel",
+            "-8",
+        ])
+        .status()
+    {
         return true;
     };
     false
